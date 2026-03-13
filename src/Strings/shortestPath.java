3e7fb7f07f98ca1112768e866afe9fd14b70a57e
Apoorv Sethi <apoorv116oorv@gmail.com>
2026-03-13 17:48:39 +0530

    wrote the file shortestPath

--- src/Strings/shortestPath.java
+++ src/Strings/shortestPath.java
@@ -1,4 +1,26 @@
 package Strings;
 
 public class shortestPath {
+    public static void main(String[] args) {
+        String str = "WNEENESENNN";
+        int x =0 , y = 0;
+        double dist =0;
+        for(int i = 0; i < str.length(); i++){
+            switch(str.charAt(i)){
+                case 'N': y++;
+                    break;
+                case 'S': y--;
+                    break;
+                case 'W': x--;
+                    break;
+                case 'E': x++;
+                    break;
+
+            }
+
+        }
+        dist = Math.hypot(x , y);
+        System.out.print(dist);
+
+    }
 }
