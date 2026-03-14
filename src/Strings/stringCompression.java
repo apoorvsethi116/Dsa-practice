325a0f52c25b4d8aaf516b678850ad09445b7699
Apoorv Sethi <apoorv116oorv@gmail.com>
2026-03-14 21:37:57 +0530

    usual

---
+++ src/Strings/stringCompression.java
@@ -0,0 +1,23 @@
+package Strings;
+import java.util.*;
+
+
+public class stringCompression {
+    public static void main(String[] args) {
+        String str = new Scanner(System.in).nextLine();
+        StringBuilder sb = new StringBuilder("");
+        for(int i = 0; i <str.length(); i++){
+            Integer count = 1;
+            while(i<str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count ++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count > 1){
+                sb.append(count);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
