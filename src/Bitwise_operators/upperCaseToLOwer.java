6e4a8ebcdfa51adcf48fc60329e906de9877695a
Apoorv Sethi <apoorv116oorv@gmail.com>
2026-03-16 18:58:56 +0530

    Bit is lit

---
+++ src/Bitwise_operators/upperCaseToLOwer.java
@@ -0,0 +1,13 @@
+package Bitwise_operators;
+
+public class upperCaseToLOwer {
+    public static void main(String[] args){
+        char ch = 'A';
+
+        char lower = (char)(ch | 32);
+
+        System.out.println(lower);
+    }
+}
+//to underswtand use of bit else where
+//https://graphics.stanford.edu/~seander/bithacks.html
