93283ba8f8ed7d4420aa3632eda0b80922e9a303
Apoorv Sethi <apoorv116oorv@gmail.com>
2026-03-21 16:50:35 +0530

    recurring

--- src/Recursion/lengthOfString.java
+++ src/Recursion/lengthOfString.java
@@ -1,7 +1,16 @@
 package Recursion;
 
 public class lengthOfString {
+    public static int lengthOfString(String str , int i ){
+        if(i == str.length()){
+            return 0;
+        }
+        return 1 + lengthOfString(str , i+1);
+
+    }
+
     public static void main(String[] args) {
         String str = "sethi";
+        System.out.println(lengthOfString(str , 0));
     }
 }
