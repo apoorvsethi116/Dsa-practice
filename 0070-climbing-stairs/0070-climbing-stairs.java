699ad18739d0859c77b3a209e14d1be12adc77d2
Apoorv Sethi <apoorv116oorv@gmail.com>
2026-08-23 23:15:19 +0530

    Time: 0 ms (100%), Space: 42.1 MB (55.05%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int ways(int n , int[] dp ){
+        if(n==1 || n==0){
+            return 1;
+        }
+
+        if(dp[n] != 0){
+            return dp[n];
+        }
+       
+      dp[n] = ways(n-1 , dp) + ways(n-2 , dp);
+
+      return dp[n];
+    }
+    public int climbStairs(int n) {
+
+        int[] dp = new int[n+1];
+        
+       return ways(n , dp);
+    }
+}
